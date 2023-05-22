4d9b846b28e9449423dc9bf88eec501f81232fcc
florius88 <florsantosbueno@gmail.com>
2023-05-22 20:30:50 +0200

    DTO User

--- src/auth/dto/update-auth.dto.ts
+++ src/auth/dto/update-auth.dto.ts
@@ -1,4 +1,6 @@
 import { PartialType } from '@nestjs/mapped-types';
-import { CreateAuthDto } from './create-auth.dto';
 
-export class UpdateAuthDto extends PartialType(CreateAuthDto) {}
+import { CreateUserDto } from './create-user.dto';
+
+
+export class UpdateAuthDto extends PartialType(CreateUserDto) {}
